b7ca1104ae0e846515e6c2336ea2d4b4a7067d53
Nolwenn123 <nolwenn.bourey@gmail.com>
2024-02-22 17:49:55 +0100

    remplissage de la bdd

--- src/main/java/fr/epsi/b32324c1/Main.java
+++ src/main/java/fr/epsi/b32324c1/Main.java
@@ -5,24 +5,22 @@ public class Main {
         System.out.println("Hello world!");
     }
     EntityManager entityManager = entityManagerFactory.createEntityManager();
-    entityManager.getTransaction().begin();
-    // Exemple d'insertion pour Product
-    Product product1 = new Product();
-    product1.setCode("code1");
-    product1.setLabel("label1");
-    product1.setType(ProdType.TYPE1);
-    product1.setPrice(10.0);
-    entityManager.persist(product1);
-    entityManager.getTransaction().commit();
-    entityManager.close();
-
     String jpql = "SELECT a FROM Animal a WHERE a.petStore = :petStore";
-    List<Animal> animals = entityManager.createQuery(jpql, Animal.class)
-            .setParameter("petStore", petStore)
-            .getResultList();
 
-    List<Animal> animals = query.getResultList();
-    entityManager.close();
+    Product product = new Product();
+        id.setId(12345);
+    Code.setCode(12345);
+    entityManager.persist(Product);
 
+        Petstore petstore = new Petstore();
+        id.setId(123);
+        name.setName("PetSheep");
+        entityManager.persist(petstore);
+
+        Animal animal = new Animal();
+        Id.setId("hjkhjk");
+        birth.setBirth("12/05/2002");
+        couleur.setCouleur("bleu");
+        entityManager.persist();
 
 }
---
+++ src/main/java/fr/epsi/b32324c1/bo/ProdType.java
@@ -0,0 +1,5 @@
+public enum ProdType {
+    FOOD,
+    ACCESSORY,
+    CLEANING
+}
